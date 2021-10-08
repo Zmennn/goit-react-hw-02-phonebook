04c62e336a26beac6b80f2e09d43d1a801806d69
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-10-08 08:57:20 +0300

    015

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    null
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+
+
+  render() {
+    return (<>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        required
+      /></>)
+  }
+
 }
 
 export default App;
